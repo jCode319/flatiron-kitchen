87f4b5f7f6de97a6134c5fb80e9e5c5cacd39dd7
jCode319 <jdjack3@gmail.com>
2020-12-09 19:54:15 -0500

    commit tree 31a7664de2fc94d2269c7ecbe80c18d8bf9400ea

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,38 @@
+class IngredientsController < ApplicationController
+
+
+  def show
+    @ingredient = Ingredient.find_by(params[:id])
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        redirect_to ingredient_path(@ingredient)
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @ingredient = Ingredient.find_by(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find_by(params[:id])
+    @ingredient.update(ingredient_params)
+    redirect_to ingredient_path(@ingredient)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+
+end
